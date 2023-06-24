5cf9aed5431be463d8a44024dcfba208f8c1ebcf
me-safty <erensafty@gmail.com>
2023-06-24 22:30:57 +0300

    update

--- store-management-application/js/header.js
+++
@@ -1,5 +0,0 @@
-// header
-const closeSidebarBtn = document.querySelector(".close-sidebar-btn");
-const sideBar = document.querySelector(".sidebar");
-
-closeSidebarBtn.onclick = () => sideBar.classList.toggle("active");
